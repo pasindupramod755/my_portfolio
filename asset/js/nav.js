acf9ecd2e1d0840de795b181578b3f763cb89012
pasindupramod755 <pasidubandara12345@gmail.com>
2025-09-10 23:47:50 +0530

    Nav Bar Load JS index

---
+++ asset/js/nav.js
@@ -0,0 +1,19 @@
+document.getElementById("navLoad").innerHTML = `
+    <nav class="bg-black">
+        <ul class="nav justify-content-end  p-1 ">
+
+            <li class="nav-item">
+                <a class="nav-link m-3 text-white" aria-current="page" href="/index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link m-3 text-white" href="/about.html">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link m-3 text-white" href="/contact.html">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link m-3 text-white" href="/project.html">Project</a>
+            </li>
+        </ul>
+    </nav>
+`
